8f35b4a1818aa49d476984d0a03dcadcd1efba37
卢梓文 <i_luziwen@cvte.com>
2021-09-01 17:30:59 +0800

    homography

--- run_customized.py
+++ run_customized.py
@@ -21,6 +21,27 @@ def compute_homography(matched_kp1, matched_kp2):
     inliers = inliers.flatten()
     return H, inliers
 
+def find_r_t(Rs, Ts, finalkp1, finalkp2):
+    if len(Rs) == 1:
+        return Rs[0], Ts[0].squeeze()
+    for j in range(len(Rs)):
+        left_projection = mint @ np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])  # world-coor
+        right_projection = mint @ np.concatenate((Rs[j], Ts[j]), axis=1)
+        triangulation = cv2.triangulatePoints(left_projection, right_projection, finalkp1[0],
+                                              finalkp2[0])  # point in world-coor
+        triangulation = triangulation / triangulation[3]  # make it homogeneous (x,y,z,1)
+        if triangulation[2] > 0:  # z is positive
+            point_in_cam2 = np.concatenate((Rs[j], Ts[j]), axis=1) @ triangulation  # change to cam2 coordinate
+            if point_in_cam2[2] > 0:  # z is positive
+                rotation = Rs[j]
+                translation = Ts[j].squeeze()
+                print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+                print(j)
+                break
+        else:
+            continue
+    return rotation, translation
+
 def main(args):
     _default_cfg = deepcopy(default_cfg)
     _default_cfg['coarse']['temp_bug_fix'] = True  # set to False when using the old ckpt
@@ -46,7 +67,7 @@ def main(args):
     imgs = imgs[0:750]
     #create a dict to record performance
     dict1 = {'match_id': [], 'detected_matches': [], 'valid_matches': [], 'inlier_rate': [], 'detection_time': [], 'EulerZ_error(degree)': [], 'EulerY_error(degree)': [], 'EulerX_error(degree)': [],
-             't1_error(m)': [], 't2_error(m)': [], 't3_error(m)': [], 'delta_R(Rodrigues)':[]}
+             't1_error(m)': [], 't2_error(m)': [], 't3_error(m)': [], 'delta_R(Rodrigues)':[], 'delta_R_ORB':[]}
 
     print(f"\nExtracting features for {img_pth}")
     # input ground truth dataframe path here
@@ -102,20 +123,8 @@ def main(args):
 
         # get correct pose, you will have four sets of solution, only one with both positive z value for two cameras
         num, Rs, Ts, Ns = cv2.decomposeHomographyMat(H, mint)
-        for j in range(len(Rs)):
-            left_projection = mint @ np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])  # world-coor
-            right_projection = mint @ np.concatenate((Rs[j], Ts[j]), axis=1)
-            triangulation = cv2.triangulatePoints(left_projection, right_projection, finalkp1[0],
-                                                  finalkp2[0])  # point in world-coor
-            triangulation = triangulation / triangulation[3]  # make it homogeneous (x,y,z,1)
-            if triangulation[2] > 0:  # z is positive
-                point_in_cam2 = np.concatenate((Rs[j], Ts[j]), axis=1) @ triangulation  # change to cam2 coordinate
-                if point_in_cam2[2] > 0:  # z is positive
-                    rotation = Rs[j]
-                    translation = Ts[j].squeeze()
-                    break
-            else:
-                continue
+        rotation, translation = find_r_t(Rs, Ts, finalkp1, finalkp2)
+
         # record error
         if args.gtcsv != '':
             delta_rotation = rotation.T @ gt_rotation
@@ -135,6 +144,35 @@ def main(args):
 
             dict1['delta_R(Rodrigues)'].append(R_err)
 
+            #you may want to compare to ORB, here is the code
+            # Initiate ORB detector
+            orb = cv2.ORB_create()
+            # find the keypoints and descriptors with ORB
+            kp1, des1 = orb.detectAndCompute(img0_raw, None)
+            kp2, des2 = orb.detectAndCompute(img1_raw, None)
+            # create BFMatcher object
+            bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+            # Match descriptors.
+            matches = bf.match(des1, des2)
+            # Sort them in the order of their distance.
+            matches = sorted(matches, key=lambda x: x.distance)
+            matches = matches[:1000]           #allow at most 1000 matches
+            src_pts = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(-1, 2)
+            dst_pts = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1, 2)
+            H2, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC)
+            num1, Rs1, Ts1, Ns1 = cv2.decomposeHomographyMat(H2, mint)
+            mask = mask.squeeze()
+            rotation_orb, translation_orb = find_r_t(Rs1, Ts1, src_pts[mask.astype(bool)], dst_pts[mask.astype(bool)])
+            delta_rotation_orb = rotation_orb.T @ gt_rotation
+            # angle error between 2 rotation matrices
+            cos1 = (np.trace(delta_rotation_orb) - 1) / 2
+            cos1 = np.clip(cos1, -1., 1.)  # handle numercial errors
+            R_err_orb = np.rad2deg(np.abs(np.arccos(cos1)))
+
+            dict1['delta_R_ORB'].append(R_err_orb)
+
+
+
         # plot
         # initialize the output visualization image
         (hA, wA) = img0_raw.shape[:2]  # cv2.imread returns (h,w,c)
@@ -145,8 +183,6 @@ def main(args):
 
         # loop over the matches
         for p1, p2 in zip(finalkp1, finalkp2):
-            # only process the match if the keypoint was successfully
-            # matched
             # draw the match
             ptA = (int(p1[0]), int(p1[1]))
             ptB = (int(p2[0]) + wA, int(p2[1]))
